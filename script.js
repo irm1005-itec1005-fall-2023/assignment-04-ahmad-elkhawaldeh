fe43187b75240f7a2bfd56b91bf7ec0367e0d2e7
Ahmad elkhawaldeh <ahmadelkhawaldeh@cmail.carleton.ca>
2023-11-29 20:41:03 -0500

    draft

--- script.js
+++ script.js
@@ -1,43 +1,153 @@
-/* Assignment 04: Finishing a Todo List App
- *
- * 
- *
- */
-
-
-//
-// Variables
-//
-
 // Constants
 const appID = "app";
-const headingText = "To do. To done. ✅";
+const headingText = " ";
 
 // DOM Elements
 let appContainer = document.getElementById(appID);
+let todoIdCounter = 0;
+let todoItems = [];
+let selectedDay = ""; // Added variable to store the selected day
 
-//
 // Functions
-//
 
 // Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
+function initialise() {
   if (!appContainer) {
-    console.error("Error: Could not find app contianer");
+    console.error("Error: Could not find app container");
     return;
   }
 
-  // Create an h1 and add it to our app
   const h1 = document.createElement("h1");
   h1.innerText = headingText;
   appContainer.appendChild(h1);
 
-  // Init complete
   console.log("App successfully initialised");
 }
 
-//
+// Add a todo to the list
+function addToDo() {
+  const inputField = document.getElementById("todoInput");
+  const todoText = inputField.value.trim();
+
+  if (todoText !== "" && selectedDay !== "") { // Check if a day is selected
+    const newTodo = {
+      id: todoIdCounter++,
+      text: todoText,
+      completed: false,
+      day: selectedDay, // Assign the selected day to the todo
+    };
+
+    todoItems.push(newTodo);
+    inputField.value = "";
+    renderToDoList();
+  }
+}
+
+// Function to remove a todo from the list
+function removeToDoItem(todoId) {
+  todoItems = todoItems.filter((todo) => todo.id !== todoId);
+  renderToDoList();
+}
+
+// Function to mark a task as completed
+function markToDoItemAsCompleted(todoId) {
+  const todo = todoItems.find((todo) => todo.id === todoId);
+
+  if (!todo) {
+    console.error(`Todo with ID ${todoId} not found.`);
+    return;
+  }
+
+  todo.completed = !todo.completed;
+  renderToDoList();
+}
+
+// Function to delete a task from the array
+function deleteToDoItem(todoId) {
+  const todoIndex = todoItems.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex === -1) {
+    console.error(`Todo with ID ${todoId} not found.`);
+    return;
+  }
+
+  todoItems.splice(todoIndex, 1);
+  renderToDoList();
+}
+
+// Function to clear all completed tasks
+function clearCompletedTasks() {
+  todoItems = todoItems.filter((todo) => !todo.completed);
+  renderToDoList();
+}
+
+// Function to render the days of the week
+function renderDaysOfWeek() {
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const daysOfWeekContainer = document.getElementById("daysOfWeek");
+
+  daysOfWeek.forEach((day) => {
+    const dayElement = document.createElement("button");
+    dayElement.textContent = day;
+    dayElement.addEventListener("click", () => handleDaySelection(day));
+    daysOfWeekContainer.appendChild(dayElement);
+  });
+}
+
+function handleDaySelection(selectedDayValue) {
+  const daysOfWeekContainer = document.getElementById("daysOfWeek");
+  const daysOfWeek = daysOfWeekContainer.children;
+
+  for (let i = 0; i < daysOfWeek.length; i++) {
+    const day = daysOfWeek[i];
+    day.classList.toggle("selected", day.textContent === selectedDayValue);
+  }
+
+  selectedDay = selectedDayValue;
+  updateSelectedDay(selectedDay);
+}
+
+function updateSelectedDay(selectedDay) {
+  const selectedDayDisplay = document.getElementById("selectedDayDisplay");
+  selectedDayDisplay.textContent = `Selected Day: ${selectedDay}`;
+}
+
+function renderToDoList() {
+  const todoListContainer = document.getElementById("todoList");
+
+  // Clear existing list items
+  todoListContainer.innerHTML = "";
+
+  // Filter tasks based on the selected day
+  const filteredTasks = todoItems.filter((todo) => todo.day === selectedDay);
+
+  // Render each filtered todo item
+  filteredTasks.forEach((todo) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("todo-item");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", () => markToDoItemAsCompleted(todo.id));
+
+    const todoText = document.createElement("text");
+    todoText.textContent = todo.text;
+    todoText.classList.add("text");
+    todoText.classList.toggle("completed", todo.completed);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => deleteToDoItem(todo.id));
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(todoText);
+    listItem.appendChild(deleteButton);
+
+    todoListContainer.appendChild(listItem);
+  });
+}
+
 // Inits & Event Listeners
-//
-inititialise();
+initialise();
+renderDaysOfWeek();
